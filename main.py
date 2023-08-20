d74321ce7c38540a7949d697c0ea97f2905d401d
SaM-92 <sam.misaqian@gmail.com>
2023-08-20 01:53:32 +0100

    main code and functions are built and tested

--- main.py
+++ main.py
@@ -3,23 +3,33 @@ from subs.functions_QRC import *
 
 # Example usage:
 
-# the website you want the QRC to generate the image for
+# Define the website URL and paths for logos and output images
+
+
+# link to the website
 website = "https://t.me/DataMasterMind"
 
-# Your logo
+# logo
 logo_path = "./Logos/logo-1.png"
 
+# outputs
+output_path_1 = "./QR_Codes/Ex-1.png"
+output_path_2 = "./QR_Codes/Ex-2.png"
+output_path_3 = "./QR_Codes/Ex-3.png"
+output_path_4 = "./QR_Codes/Ex-4.png"
+output_path_5 = "./QR_Codes/Ex-5.png"
 
-# output path and output names for each function
-output_path_1 = "./QR_Codes/Ex-1-teste.png"
-output_path_2 = "./QR_Codes/Ex-2-qrc-with-backgrounds.png"
-# output_path_3 = "./QR_Codes/Ex-3-colormask-qrcode.png"
-
-# basic QRC
+# Generate a basic QR code
 img_1 = generate_basic_qr_code(website, output_path_1)
 
-# Creating a Simple QR Code with a Link on the Background Image
+# Generate a QR code with a background image
 img_2 = generate_qr_code_with_background(website, logo_path, output_path_2)
 
+# Generate a QR code with a color mask
+img_3 = generate_qr_code_with_colormask(website, logo_path, output_path_3)
 
-# img_3 = generate_qr_code_with_colormask(website, logo_path, output_path_3)
+# Generate a QR code with a custom logo overlay
+img_4 = generate_qr_code_with_custom_position(website, logo_path, output_path_4)
+
+# Generate a custom styled QR code with transparency and gradient background
+img_5 = generate_custom_styled_qr_code(website, logo_path, output_path_5)
--- subs/functions_QRC.py
+++ subs/functions_QRC.py
@@ -81,6 +81,52 @@ from qrcode.image.styles.moduledrawers import (
 )
 
 
+def generate_qr_code_with_custom_position(
+    website, logo_path, output_path, fill_color="white", back_color="black"
+):
+    """
+    Generate a QR code with a custom position for the logo.
+
+    :param website: The website URL for the QR code.
+    :param logo_path: Path to the logo image.
+    :param output_path: Output path to save the QR code image.
+    :param fill_color: Fill color of the QR code.
+    :param back_color: Background color of the QR code.
+    :return: The generated QR code image.
+    """
+    # Create a QR code instance
+    qr = qrcode.QRCode(
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+
+    # Add data to the QR code
+    qr.add_data(website)
+    qr.make(fit=True)
+    # Generate QR code image with specified fill and background colors
+    qr_code_img = qr.make_image(fill_color=fill_color, back_color=back_color)
+
+    # Open and enhance the logo
+    logo = Image.open(logo_path)
+    logo = logo.resize((100, 100))  # Resize the logo as needed
+    logo_with_transparency = logo.copy()
+    enhancer = ImageEnhance.Brightness(logo_with_transparency)
+    logo_with_transparency = enhancer.enhance(0.7)
+
+    # Calculate the position to overlay the logo in the center
+    qr_width, qr_height = qr_code_img.size
+    logo_width, logo_height = logo_with_transparency.size
+    position = ((qr_width - logo_width) // 2, (qr_height - logo_height) // 2)
+
+    # Paste the logo onto the QR code image at the calculated position
+    qr_code_img.paste(logo_with_transparency, position, logo_with_transparency)
+
+    # Save the final QR code image
+    qr_code_img.save(output_path)
+    return qr_code_img
+
+
 def generate_custom_styled_qr_code(
     website, logo_path, output_path, fill_color="white", back_color="black"
 ):
@@ -113,6 +159,7 @@ def generate_custom_styled_qr_code(
 
     # Open and enhance the logo
     logo = Image.open(logo_path)
+    logo = logo.resize((100, 100))  # Resize the logo as needed
     logo_with_transparency = logo.copy()
     enhancer = ImageEnhance.Brightness(logo_with_transparency)
     logo_with_transparency = enhancer.enhance(0.7)
@@ -128,43 +175,3 @@ def generate_custom_styled_qr_code(
     # Save the final QR code image
     blended_image.save(output_path)
     return blended_image
-
-
-def generate_qr_code_with_custom_position(
-    website, logo_path, output_path, position, fill_color="white", back_color="black"
-):
-    """
-    Generate a QR code with a custom position for the logo.
-
-    :param website: The website URL for the QR code.
-    :param logo_path: Path to the logo image.
-    :param output_path: Output path to save the QR code image.
-    :param position: Custom position tuple (x, y) for the logo.
-    :param fill_color: Fill color of the QR code.
-    :param back_color: Background color of the QR code.
-    :return: The generated QR code image.
-    """
-    # Create a QR code instance
-    qr = qrcode.QRCode(
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=10,
-        border=4,
-    )
-    # Add data to the QR code
-    qr.add_data(website)
-    qr.make(fit=True)
-    # Generate QR code image with specified fill and background colors
-    qr_code_img = qr.make_image(fill_color=fill_color, back_color=back_color)
-
-    # Open and enhance the logo
-    logo = Image.open(logo_path)
-    logo_with_transparency = logo.copy()
-    enhancer = ImageEnhance.Brightness(logo_with_transparency)
-    logo_with_transparency = enhancer.enhance(0.7)
-
-    # Paste the logo onto the QR code image at the custom position
-    qr_code_img.paste(logo_with_transparency, position, logo_with_transparency)
-
-    # Save the final QR code image
-    qr_code_img.save(output_path)
-    return qr_code_img
